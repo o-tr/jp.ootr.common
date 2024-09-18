c4580de3971ba913f377112ea01ed2b7431cf744
ootr <git@ootr.jp>
2024-09-18 16:31:08 +0900

    DateTimeUtilsのテストを実装

---
+++ Runtime/UnityEditorTests/DateTime.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace jp.ootr.common.Tests.DateTimeUtils
+{
+    public static class ToUnixTimeTests
+    {
+        [Test]
+        public static void ToUnixTime()
+        {
+            var dt = new System.DateTime(2021, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
+            Assert.AreEqual(1609459200, dt.ToUnixTime());
+        }
+    }
+}
